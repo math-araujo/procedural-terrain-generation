efbbb8af907fda5ecdb24dcd68d7f3be4253a057
M-A-rs <araujo_m_@hotmail.com>
2022-03-16 23:15:10 -0300

    Add initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,67 @@
+// GLAD must be imported before GLFW
+// clang-format off
+#include <glad/glad.h>
+// clang-format on
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <iostream>
+
+void error_callback(int error, const char* description)
+{
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
+
 int main()
 {
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failure to initialize GLFW" << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    const int width = 800;
+    const int height = 600;
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "Test", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failure to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failure to initialize GLAD" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Clear window with specified color
+        glClearColor(0.0f, 0.1f, 0.4f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
