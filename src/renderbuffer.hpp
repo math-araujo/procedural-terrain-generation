7aa420d148318fddb8ed24ff619e4345b0155afd
math-araujo <araujo_m@usp.br>
2022-09-09 21:01:47 -0300

    Add Renderbuffer class

---
+++ src/renderbuffer.hpp
@@ -0,0 +1,27 @@
+#ifndef RENDERBUFFER_HPP
+#define RENDERBUFFER_HPP
+
+#include <cstdint>
+
+#include <glad/glad.h>
+
+class Renderbuffer
+{
+public:
+    Renderbuffer(std::uint32_t width, std::uint32_t height, GLenum internal_format);
+    Renderbuffer(const Renderbuffer&) = delete;
+    Renderbuffer(Renderbuffer&& other) noexcept;
+    Renderbuffer& operator=(const Renderbuffer&) = delete;
+    Renderbuffer& operator=(Renderbuffer&&) noexcept;
+    ~Renderbuffer();
+
+    std::uint32_t id() const;
+    std::uint32_t width() const;
+    std::uint32_t height() const;
+private:
+    std::uint32_t width_;
+    std::uint32_t height_;
+    std::uint32_t id_{0};
+};
+
+#endif // RENDERBUFFER_HPP
